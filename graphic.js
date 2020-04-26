2264d33dfaaff1e1e618477d2dfa26d28cbd9695
Tig0r <christopher.putz@sheep-records.de>
2020-04-26 12:57:30 +0200

    changed image to enlargen it a little, also text alignment is now automatic

--- graphic.js
+++ graphic.js
@@ -16,34 +16,53 @@ module.exports =
             context.drawImage(image, 0, 0, width, height);
             if(island.title)
             {
+                const xpos = titleconfig.xpos + titleconfig.width / 2;
+                const ypos = titleconfig.ypos + titleconfig.height / 2;
                 context.font = titleconfig.fontsize + " Comic Sans MS";
                 context.fillStyle = "white";
                 context.textAlign = "center";
-                context.fillText(island.title, titleconfig.xpos, titleconfig.ypos);
+                context.textBaseline = "middle";
+                context.fillText(island.title, xpos, ypos, titleconfig.width);
             }
             
             if(island.name)
             {
-                context.font = nameconfig.fontsize + " Comic Sans MS";
+                let xpos = nameconfig.xpos + nameconfig.width / 2;
+                let ypos = nameconfig.ypos + nameconfig.height / 2;
+                let fontsize = nameconfig.fontsize;
+                if(!island.title)
+                {
+                    xpos = titleconfig.xpos + titleconfig.width / 2;
+                    ypos = titleconfig.ypos + (titleconfig.height + nameconfig.height) / 2;
+                    fontsize = nameconfig.alternativeFontsize;
+                }
+                context.font = fontsize + " Comic Sans MS";                
                 context.fillStyle = "white";
                 context.textAlign = "center";
-                context.fillText(island.name, nameconfig.xpos, nameconfig.ypos);
+                context.textBaseline = "middle";
+                context.fillText(island.name, xpos, ypos, nameconfig.width);
             }
 
             if(island.island)
             {
+                const xpos = islandconfig.xpos + islandconfig.width / 2;
+                const ypos = islandconfig.ypos + islandconfig.height / 2;
                 context.font = islandconfig.fontsize + " Comic Sans MS";
                 context.fillStyle = "white";
                 context.textAlign = "center";
-                context.fillText(island.island, islandconfig.xpos, islandconfig.ypos);
+                context.textBaseline = "middle";
+                context.fillText(island.island, xpos, ypos, islandconfig.width);
             }
 
             if(island.dodocode)
             {
+                const xpos = dodocodeconfig.xpos + dodocodeconfig.width / 2;
+                const ypos = dodocodeconfig.ypos + dodocodeconfig.height / 2;
                 context.font = dodocodeconfig.fontsize + " Comic Sans MS";
                 context.fillStyle = "white";
                 context.textAlign = "center";
-                context.fillText(island.dodocode, dodocodeconfig.xpos, dodocodeconfig.ypos);
+                context.textBaseline = "middle";
+                context.fillText(island.dodocode, xpos, ypos, dodocodeconfig.width);
             }
 
             loadImage('./dodo_code_lines.png').then(image => {
